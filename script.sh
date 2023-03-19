a486086bbe111c019844502ec645be91941bd35d
TarikVUT <tarikalkanan@gmail.com>
2023-03-19 15:01:51 +0100

    update

--- script.sh
+++ script.sh
@@ -16,6 +16,7 @@ then
 	echo "---> Installation $message_to_user was successful<---"
 else
 	echo "*** Installation $message_to_user failed ***"
+	return 0
 fi
 }
 
@@ -26,13 +27,29 @@ then
         check_command_success "sudo dnf install virtualenv -y" "virtualenv"
 
 	#Download source from Githut
-	check_command_success "git clone -b sapp https://github.com/forsenior/os.git $dir_to_install/OS" "sapp"
+	check_command_success "git clone https://github.com/TarikVUT/semestral.git $dir_to_install/OS" "sapp"
+	#check_command_success "git clone -b sapp https://github.com/forsenior/os.git $dir_to_install/OS" "sapp"
 	
 	#Change OS owner
 	check_command_success "sudo chown -R $user /home/$user/OS" "OS"
 	
-	#Move sapp to /home/
-	check_command_success "sudo mv $dir_to_install/OS/sapp /home/root" "trans"
+	#Move sapp,stext to /home/
+	check_command_success "sudo mv $dir_to_install/OS/sapp /home/root" "trans sapp"
+	check_command_success "sudo mv $dir_to_install/OS/stext /home/root" "trans stext"
+	
+	#Create virtual env inside sapp,stext,sweb,smail.
+	check_command_success "virtualenv /home/root/sapp/env" "create sapp env"
+	check_command_success "virtualenv /home/root/stext/env" "create sapp env"
+	#check_command_success "sudo chown -R $user /home/root/sapp" "sapp"
+	source /home/root/sapp/env/bin/activate && pip install pillow pygame 
+	
+	source /home/root/stext/env/bin/activate && pip install pyqt5 xhtml2pdf  bs4 markdown
+	#pip install pillow pygame ,pyqt5 xhtml2pdf  bs4 markdown
+	
+	#Create autostart file to run sapp
+	
+	
+	# disable window button
 
 else
         echo "ERROR"
